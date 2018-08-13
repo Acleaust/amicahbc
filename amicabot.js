4f9f38467ecfac90117df9c25f0780810e18ed24
Acleaust <acleaust@gmail.com>
2018-08-14 00:06:22 +0300

    Edistystä

--- amicabot.js
+++ amicabot.js
@@ -1,23 +1,28 @@
-//Amicabotsi
+//
+// Fazer Food & Co Pääraide bot
+//
+
 const TeleBot = require('telebot');
 var jp = require('jsonpath');
 
 //Muuttujat
-const rssurl = "http://www.amica.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=0083&language=fi"
+const rssurl = "https://www.fazerfoodco.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=0083&language=fi"
 var fs = require('fs');
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-
 var schedule = require('node-schedule')
-
 const adapter = new FileSync('data.json')
-const db = low(adapter)
+const dbjson = low(adapter)
+//Rss parser
+const feedparser = require('feedparser-promised');
+
+var striptags = require('striptags');
 
 //BotToken
 const bot = new TeleBot('TOKEN');
 
 // Set some defaults
-db.defaults({ users: [] })
+dbjson.defaults({ users: [] })
     .write()
 
 // Logaa jokaisen sisääntulevan viestin consoliin
@@ -26,33 +31,83 @@ bot.on('text', function (msg) {
 });
 
 //Komennot
+
+// /start
 bot.on('/start', (msg) => {
+    //iid = chatin ID
     var iid = msg.chat.id
 
+    //Hakee data.jsonin
     var obj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    //Ettii data.jsonista kaikki ID:t
     var iideet = jp.query(obj, '$..id')
 
+    //Jokainen id käydään läpi ja tarkistetaan onko samaa id:tä jo rekisteröity
     for (i = 0; i < iideet.length; i += 1) {
         var iidee = iideet[i]
         if (iid == iidee) {
             console.log("Dataa ei tallenneta")
-            return bot.sendMessage(msg.chat.id, `Hei, ${msg.from.first_name}! Saat tästä lähtien päivän safkat suoraan Telegramiin!`); //Vastaa kun käyttäjä käyttää /start komentoa
+            return bot.sendMessage(msg.chat.id, `Hei, ${msg.from.first_name}! Saat tästä lähtien päivän safkat suoraan Telegramiin!\n\nVoit lopettaa tilauksen tekemällä /stop.`); //Vastaa kun käyttäjä käyttää /start komentoa
         } else {
             //älä tee mitää
         }
     }
     console.log("Data tallennetaan")
     // Data is automatically saved to localStorage
-    db.get('users')
+    dbjson.get('users')
         .push({ id: iid })
         .write()
 
-    return bot.sendMessage(msg.chat.id, `Hei, ${msg.from.first_name}! Saat tästä lähtien päivän safkat suoraan Telegramiin!`); //Vastaa kun käyttäjä käyttää /start komentoa
+    return bot.sendMessage(msg.chat.id, `Hei, ${msg.from.first_name}! Saat tästä lähtien päivän safkat suoraan Telegramiin!\n\nVoit lopettaa tilauksen tekemällä /stop.`); //Vastaa kun käyttäjä käyttää /start komentoa
 });
 
-var j = schedule.scheduleJob('5 * * * *', function () {
-    return bot.sendMessage(81023943, `Hei`)
-    console.log('The answer to life, the universe, and everything!');
+// /stop
+bot.on('/stop', (msg) => {
+    var iid = msg.chat.id
+
+    var array = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    //var datajson = fs.readFileSync('data.json', 'utf8');
+    var iideet = jp.query(array, '$..id')
+    for (i = 0; i < iideet.length; i += 1) {
+        if (iideet[i] == iid) {
+
+            dbjson.get('users')
+                .splice(i, 1)
+                .write()
+
+            console.log(iid + " Unsubbed")
+            return bot.sendMessage(msg.chat.id, `Se on sitten moro ${msg.from.first_name}!`).catch(err => console.log(err));
+        } else {
+            //älä tee mitää
+        }
+    }
+})
+
+// TESTI
+//Lähettää viestin joka minuutti 
+var j = schedule.scheduleJob('30 * * * * *', function () {
+
+    feedparser.parse(rssurl).then((items) => {
+        var rsstitle = jp.query(items, '$..title')
+        var rssdescription = jp.query(items, '$..description')
+
+        var rssdescription = rssdescription.replace('<br>', '')
+        console.log(rssdescription)
+
+        var dbjson = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+        var iideet = jp.query(dbjson, '$..id')
+
+        for (i = 0; i < iideet.length; i += 1) {
+            var iidee = iideet[i]
+            bot.sendMessage(iidee, rsstitle + '\nTänään ruokana:\n\n' + rssdescription)
+                .then((response) => {
+                    //Do nothing
+                }).catch((error) => {
+                    console.log('Error:', error);
+                });
+        }
+        return console.log('[info] Ruokalista lähetetty!');
+    }).catch(error => console.error('error: ', error));
 });
 
 //Ohjelman pyöritys
